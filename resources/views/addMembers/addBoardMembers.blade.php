1ca54bde1383ab186c48d65737395f92b229f53b
fumbuka1998 <limbufumbuka@gmail.com>
2023-03-25 14:52:40 +0300

    adding the board member interfaces

--- resources/views/addMembers/addBoardMembers.blade.php
+++ resources/views/addMembers/addBoardMembers.blade.php
@@ -1,1 +1,378 @@
-jjssjsjjsjs
+@extends('layouts.addboard')
+
+@section('content')
+    <!-- add student model -->
+    <div class="modal fade" id="addStudentModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Add Board Member</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    {{-- display error validations --}}
+                    <ul id="saveform_errlist"></ul>
+
+                    <div class="form-control mb-3">
+                        <label for="">Member Name</label>
+                        <input type="text" class="name form-control">
+                    </div>
+                    <div class="form-control mb-3">
+                        <label for="">Member Email</label>
+                        <input type="text" class="email form-control">
+                    </div>
+                    <div class="form-control mb-3">
+                        <label for="">Member Pic</label>
+                        <input type="file" class="phone form-control">
+                    </div>
+                    
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary add_student">Add Student</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- end of add student modal --}}
+
+    <!-- edit student model -->
+    <div class="modal fade" id="editStudentModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Edit/Update Student</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    {{-- display error validations --}}
+                    <ul id="updateform_errlist"></ul>
+
+                    {{-- <input type="text" id="edit_student_id"> --}}
+
+                    <input type="hidden" id="edit_student_id" class="id form-control">
+
+                    <div class="form-control mb-3">
+                        <label for="">Name</label>
+                        <input type="text" id="edit_name" class="name form-control">
+                    </div>
+                    <div class="form-control mb-3">
+                        <label for="">Email</label>
+                        <input type="text" id="edit_email" class="email form-control">
+                    </div>
+                    <div class="form-control mb-3">
+                        <label for="">Phone</label>
+                        <input type="text" id="edit_phone" class="phone form-control">
+                    </div>
+                    <div class="form-control mb-3">
+                        <label for="">Course</label>
+                        <input type="text" id="edit_course" class="course form-control">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary update_student">Update</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- end of edit student modal --}}
+
+
+    <!-- delete student model -->
+    <div class="modal fade" id="deleteStudentModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Delete Student</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+
+
+                    <input type="hidden" id="delete_student_id" class="id form-control">
+                    <h3>are you sure you want to delete this student?</h3>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary delete_student_btn">Yes Delete</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- end of delete student modal --}}
+
+    <div class="container py-5">
+        <div class="row">
+            <div class="container text-md-center">
+                <h2 class="barge bg-dark text-light">
+                    BOARD MEMBER DASHBOARD
+                </h2>
+            </div>
+
+            <div class="col-md-12">
+                {{-- the div tag below display the  success message from ajax response --}}
+                <div id="success_message"></div>
+
+                <div class="card">
+                    <div class="card-header">
+                        <h4>
+                            BoardMember Data
+                            <a href="#" class="btn btn-primary float-end btn-sm" data-bs-toggle="modal"
+                                data-bs-target="#addStudentModal">Add BoardMember</a>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+
+                        <table class="table table-bordered  table-striper table-hover">
+                            <thead>
+                                <tr>
+                                    <th>MEMBER ID</th>
+                                    <th>MEMBER NAME</th>
+                                    <th>MEMBER EMAIL</th>
+                                    <th>MEMBER PIC</th>
+                                    <th>EDIT</th>
+                                    <th>DELETE</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+@endsection
+
+@section('scripts')
+    <script>
+        $(document).ready(function() {
+
+            fetchStudents()
+            //ajax function to get data from the database
+            function fetchStudents() {
+                $.ajax({
+                    type: 'GET',
+                    url: '/fetchstudents',
+                    dataType: 'json',
+                    success: function(response) {
+
+                        $('tbody').html(""); //empty he table first
+                        // alert(1);
+                        // console.log(response.students);
+                        $.each(response.students, function(key, item) {
+                            $('tbody').append(
+                                `<tr>
+                                    <td>` + item.id + `</td>
+                                    <td>` + item.name + `</td>
+                                    <td>` + item.email + `</td>
+                                    <td>` + item.phone + `</td>
+                                    <td>` + item.course + `</td>
+                                    <td><button type="button" value="` + item.id + `"
+                                            class="edit_student btn btn-primary btn-sm">edit</button></td>
+                                    <td><button type="button" value="` + item.id + `"
+                                            class="delete_student btn btn-danger btn-sm">delete</button></td>
+                                </tr>`
+                            );
+                        });
+
+                    }
+                });
+            }
+
+            // an ajax function to delete data from the list
+            $(document).on('click', '.delete_student', function(e) {
+                e.preventDefault();
+
+                var std_id = $(this).val();
+                // alert(std_id);
+                $('#delete_student_id').val(std_id);
+                $('#deleteStudentModal').modal('show');
+
+            });
+            $(document).on('click', '.delete_student_btn', function(e) {
+                e.preventDefault();
+
+                $(this).text('deleting');
+                var std_id = $('#delete_student_id').val();
+
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+
+                $.ajax({
+                    type: 'delete',
+                    url: '/delete-student/' + std_id,
+                    success: function(response) {
+                        // alert(response)
+                        $('#success_message').text(response.message);
+                        $('#success_message').addClass('alert alert-success');
+                        $('#deleteStudentModal').modal('hide');
+                        setTimeout(function() {
+                            $("#success_message").text("").removeClass();
+                        }, 5000);
+                        $('.delete_student_btn').text('Yes Delete');
+
+                        fetchStudents();
+                    }
+                });
+
+
+            });
+            //function below edit students by id
+            $(document).on('click', '.edit_student', function(e) {
+                e.preventDefault();
+                var std_id = $(this).val();
+                // console.log(std_id);
+                // alert(1);
+                $('#editStudentModal').modal('show');
+                //ajax get request to fetch data to be edited
+                $.ajax({
+                    method: "GET",
+                    url: "/edit-student/" + std_id,
+                    type: "JSON",
+                    success: function(response) {
+                        // console.log(response);
+                        if (response.status == 404) {
+                            $('#success_message').html("");
+                            $('#success_message').addClass('alert alert-danger');
+                            $('#success_message').text(response.message);
+                        } else {
+                            $('#edit_name').val(response.student.name);
+                            $('#edit_email').val(response.student.email);
+                            $('#edit_phone').val(response.student.phone);
+                            $('#edit_course').val(response.student.course);
+                            $('#edit_student_id').val(std_id);
+                        }
+                    }
+
+                });
+            });
+
+            //update function appear below 
+            $(document).on('click', '.update_student', function(e) {
+                e.preventDefault();
+                $(this).text('Updating');
+                var std_id = $('#edit_student_id').val();
+
+                var ed_data = {
+                    'name': $('#edit_name').val(),
+                    'email': $('#edit_email').val(),
+                    'phone': $('#edit_phone').val(),
+                    'course': $('#edit_course').val(),
+
+                }
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+
+                $.ajax({
+                    type: 'PUT',
+                    url: '/update_student/' + std_id,
+                    data: ed_data,
+                    dataType: 'json',
+                    success: function(response) {
+                        // alert(1);
+                        // console.log(response);
+                        if (response.status == 400) {
+                            $('#updateform_errlist').html("");
+                            $('#updateform_errlist').addClass('alert alert-danger');
+                            $.each(response.error, function(key, err_values) {
+                                // console.log(err_values);
+                                $('#updateform_errlist').append(`<li>` + err_values +
+                                    `</li>`);
+
+                            });
+                            $('.update_student').text('Update');
+
+                            setTimeout(function() {
+                                $("#updateform_errlist").text("").removeClass();
+                            }, 5000);
+
+                        } else if (response.status == 404) {
+                            $('#updateform_errlist').html("");
+                            $('#success_message').addClass('alert alert-success');
+                            $('#success_message').text(response.message);
+
+                        } else {
+                            $('#updateform_errlist').html("");
+                            $('#success_message').html("");
+                            $('#success_message').addClass('alert alert-success');
+                            $('#success_message').text(response.message);
+                            $('.update_student').text('Update');
+
+                            $('#editStudentModal').modal('hide');
+
+                            fetchStudents();
+
+                        }
+                    }
+                });
+            })
+
+            //below function add students to list
+            $(document).on('click', '.add_student', function(e) {
+                e.preventDefault();
+                // alert('tunafika hapa');
+                //below variable take data from the input field using jquery
+                var data = {
+                    'name': $('.name').val(),
+                    'email': $('.email').val(),
+                    'phone': $('.phone').val(),
+                    'course': $('.course').val()
+                };
+
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+                // console.log(data)
+                $.ajax({
+                    type: 'POST',
+                    url: '/studentstore',
+                    data: data,
+                    dataType: 'json',
+                    success: function(response) {
+                        // console.log(response);
+                        if (response.status == 400) {
+                            $('#saveform_errlist').html("");
+                            $('#saveform_errlist').addClass('alert alert-danger');
+                            $.each(response.error, function(key, err_values) {
+                                // console.log(err_values);
+                                $('#saveform_errlist').append(`<li>` + err_values +
+                                    `</li>`);
+
+                            });
+                            setTimeout(function() {
+                                $("#saveform_errlist").text("").removeClass();
+                            }, 5000);
+                        } else {
+
+                            $('#saveform_errlist').html("");
+                            $('#success_message').addClass('alert alert-success');
+                            $('#success_message').text(response.message);
+                            $('#addStudentModal').modal('hide');
+                            $('#addStudentModal').find('input').val("");
+                            fetchStudents();
+                            //function to set time out for the success message
+                            setTimeout(function() {
+                                $("#success_message").text("").removeClass();
+                            }, 5000);
+
+                        }
+                    }
+                });
+            });
+        });
+    </script>
+@endsection
---
+++ resources/views/layouts/addboard.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>myapp</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="app">
+            <main class="py-4">
+                @yield('content')
+            </main>
+        </div>
+        
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
+        @yield('scripts')
+    </body>
+</html>
